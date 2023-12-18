758e91515798a5c51f3906b94877103784187d17
mrlmhl <meralorguc1@gmail.com>
2023-12-18 14:28:12 +0300

    first

---
+++ src/components/Doctors.jsx
@@ -0,0 +1,28 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {doctorData} from "../helper/data";
+
+
+const Doctors = () => {
+  return (
+    <Container className="p-2">
+      <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
+        Our Doctors
+      </h3>
+      <Row>
+    
+        {doctorData.map(({id,img,dep,name})=> (
+            <Col key={id}>
+            <img src={img} alt={name} className="img-thumbnail doctor-img" />
+            <h5>{name}</h5>
+            <h6>{dep}</h6> 
+            
+            </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default Doctors
