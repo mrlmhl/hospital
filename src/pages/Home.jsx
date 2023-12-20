9cf820764141f52682fa14300790bd621659bbee
mrlmhl <meralorguc1@gmail.com>
2023-12-20 14:03:11 +0300

    endd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,19 +5,24 @@ import { appointmentData } from "../helper/data"
 
 const Home = () => {
 
-const [appointments, setAppointments] = useState(appointmentData);
+const [appointments, setAppointments] = useState(
+  JSON.parse(localStorage.getItem("list"))|| []);
 const handleAdd= (newAppointment)=>{
   setAppointments([...appointments, newAppointment]);
+  localStorage.setItem("list",JSON.stringify([...appointments, newAppointment]))
 };
 const handleDelete =(id)=>{
   const filteredList = appointments.filter((item)=> item.id !== id);
   setAppointments(filteredList)
+  localStorage.setItem("list", JSON.stringify(filteredList))
 };
+
 const handleDoubleClick = (id) =>{
   const updatedList =appointments.map((item)=>
   item.id===id ? {...item,consulted: !item.consulted} : item
   )
   setAppointments(updatedList)
+  localStorage.setItem("list", JSON.stringify(updatedList))
 }
   return (
     <main className="text-center mt-2">
